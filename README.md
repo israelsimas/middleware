fd677da39be3058924509c3b42f34781cb841f65
Your Name <you@example.com>
2020-03-21 18:58:52 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Middleware
+
+Library for access to mosquitto middleware.
+
+## Command to create a project 
+
+mkdir build && cd build/ && cmake ../
+
+## SUBLIME ENVIROMENT
+## Command to create a project in sublime
+
+mkdir build && cd build/ && cmake -G "Sublime Text 2 - Unix Makefiles" ../
---
+++ lib/src/middleware.c
@@ -0,0 +1,24 @@
+/**************************************************************************
+ *
+ * middleware.c
+ *
+ *    Middlware wrapper to Mosquitto.
+ *
+ * Copyright 2020
+ *
+ **************************************************************************/
+
+#include <ctype.h>
+#include <middleware.h>
+#include <utils.h>
+
+#define THIS_FILE "middleware.c"
+
+bool middleware_open() {
+
+  return true;
+}
+
+void middleware_close() {
+
+}
